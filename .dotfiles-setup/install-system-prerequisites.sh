6d8ec68a3a9c3d776c3a3c23db40de031f3520df
Camilla <>
2025-06-20 12:38:34 -0400

    fixed broken zshrc, more installer updates

--- .dotfiles-setup/install-system-prerequisites.sh
+++ .dotfiles-setup/install-system-prerequisites.sh
@@ -1,4 +1,4 @@
-sudo pacman -S git python python-sh python-termcolor rustup hwinfo choose ripgrep fd gum fzf yazi zoxide btop vim ghostty rofi-wayland dolphin keepassxc xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring zsh lazygit lsd wl-clipboard ouch yadm python-j2cli
+sudo pacman -S git python python-sh python-termcolor rustup hwinfo choose ripgrep fd gum fzf yazi zoxide btop vim ghostty rofi-wayland dolphin keepassxc xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring zsh lazygit lsd wl-clipboard ouch yadm python-j2cli polkit-kde-agent
 rustup default stable
 rustup target add x86_64-pc-windows-msvc
 
