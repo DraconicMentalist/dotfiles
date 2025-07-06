13ae81d1260d23d7a645b95e6e4f50f6e6e8036d
Camilla <>
2025-07-06 12:00:32 -0400

    added bin-base and niri install script

--- .dotfiles-setup/install-system-prerequisites.sh
+++ .dotfiles-setup/install-system-prerequisites.sh
@@ -1,4 +1,4 @@
-sudo pacman -S git python python-sh python-termcolor rustup hwinfo choose ripgrep fd gum fzf yazi zoxide btop vim ghostty rofi-wayland dolphin keepassxc xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring zsh lazygit lsd wl-clipboard ouch yadm python-j2cli mate-polkit qt6ct-kde neovide fuse2 man-db python-requests
+sudo pacman -S git python python-sh python-termcolor rustup hwinfo choose ripgrep fd gum fzf yazi zoxide btop vim ghostty rofi-wayland dolphin keepassxc xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring zsh lazygit lsd wl-clipboard ouch yadm python-j2cli mate-polkit qt6ct-kde neovide fuse2 man-db python-requests mpd mpc rmpc
 rustup default stable
 rustup target add x86_64-pc-windows-msvc
 
